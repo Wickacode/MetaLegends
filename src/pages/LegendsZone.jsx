c17bb66721f8ebc452594303ab18ce47698bf60b
Jessica Garrido <jessica.garrido.di@gmail.com>
2023-04-27 13:45:02 +0200

    Finalisation de la section MetaLegends, avec texte et bouton (non actif)

--- src/pages/LegendsZone.jsx
+++ src/pages/LegendsZone.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import AvatarLegendsZone from "../assets/images/icone/avatar-legend-zone.svg";
-import LegendsZoneText from "../assets/images/icone/legends-zone.svg"
+import LegendsZoneText from "../assets/images/icone/legends-zone.svg";
 
 export default function LegendsZone() {
   return (
     <div className="legendsZoneContainer">
-        <h2 className="title-with-gradient legendsZoneTitle">Legends Zone </h2>
-        <div className="legendsZoneImg">
-            <img className= "legendsZoneImg01" src={AvatarLegendsZone} alt="" />
-            <img src={LegendsZoneText} alt="" />
-        </div>
+      <h2 className="title-with-gradient legendsZoneTitle">Legends Zone </h2>
+      <div className="legendsZoneImg">
+        <img className="legendsZoneImg01" src={AvatarLegendsZone} alt="" />
+        <img src={LegendsZoneText} alt="" />
+      </div>
+      <p className="legendsZoneText">
+        The Legends Zone is a private space reserved to Meta Legends holders to
+        receive exciting airdrops and exclusive bonuses but also keep track of
+        their perks, benefits and advancements as holders.
+      </p>
+
+      <button className="btnBecome">Become <strong>a legend</strong></button>
     </div>
   );
 }
